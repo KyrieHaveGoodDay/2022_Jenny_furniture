781b786772b18a6f8cd6a9f9826dc7806ef69588
kyriehavegoodday <healthymbear@gmail.com>
2022-05-22 21:07:33 +0800

    更新了其他區塊

--- all.js
+++ all.js
@@ -1,6 +1,24 @@
 var swiper = new Swiper(".mySwiper", {
     // api 功能
-    slidesPerView:4.5,
-    loop:true
-    
+    slidesPerView: 1.5,
+    loop: true,
+    breakpoints: {
+        1000: {
+            slidesPerView: 4.5,
+        },
+        768: {
+            slidesPerView: 3.5,
+
+        },
+        500: {
+            slidesPerView: 2.5,
+
+        }
+    },
+    navigation: {
+        nextEl: ".prev",
+        prevEl: ".next",
+      },
+
 });
+
